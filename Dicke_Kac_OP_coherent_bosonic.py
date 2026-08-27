2bb00b76d50123c2bd99c775eb06b458bd473c8a
GB <gbquantum23@gmail.com>
2026-08-27 16:56:16 +0530

    Update Dicke_Kac_OP_coherent_bosonic.py

--- Dicke_Kac_OP_coherent_bosonic.py
+++ Dicke_Kac_OP_coherent_bosonic.py
@@ -44,9 +44,7 @@ def collective_ops(N):
     Sp = qt.jmat(N/2, '+')
     Sm = Sp.dag()
 
-    HB = Sz + (N/2)*qt.qeye(N+1)
-
-    return Sx, Sy, Sz, Sp, Sm, HB
+    return Sx, Sy, Sz, Sp, Sm
 
 ############################
 # Dicke Hamiltonian
@@ -59,8 +57,9 @@ def dicke_fun(N, nmax, ω, ω0, g):
 
     Sx, Sy, Sz, Sp, Sm, HB = collective_ops(N)
     IB = qt.qeye(N + 1)
+    HB = ω0 * (Sz + (N/2)*qt.qeye(N+1))
     
-    H = ω  * qt.tensor(a.dag() * a, IB) + ω0 * qt.tensor(Ic, HB) + (g / np.sqrt(N)) * qt.tensor(a + a.dag(), Sp + Sm)
+    H = ω  * qt.tensor(a.dag() * a, IB) + qt.tensor(Ic, HB) + (g / np.sqrt(N)) * qt.tensor(a + a.dag(), Sp + Sm)
 
     return H, HB, Sx, Sy, Sz
 
