52c7b1110d4474712e5b9409ede4fe367a21ee00
GB <gbquantum23@gmail.com>
2026-08-28 13:31:48 +0530

    Refactor Tavis-Cummings Hamiltonian computation

--- TC_Kac_OP_coherent_bosonic.py
+++ TC_Kac_OP_coherent_bosonic.py
@@ -44,9 +44,7 @@ def collective_ops(N):
     Sp = qt.jmat(N/2, '+')
     Sm = Sp.dag()
 
-    HB = Sz + (N/2)*qt.qeye(N+1)
-
-    return Sx, Sy, Sz, Sp, Sm, HB
+    return Sx, Sy, Sz, Sp, Sm
 
 ############################
 # Tavis-Cummings Hamiltonian
@@ -54,8 +52,10 @@ def collective_ops(N):
 
 def tavis_cummings(N, nmax, ω, ω0, g):
     a = qt.destroy(nmax)
-    Sx, Sy, Sz, Sp, Sm, HB = collective_ops(N)
-    H = (ω * qt.tensor(a.dag()*a, qt.qeye(N+1)) + ω0 * qt.tensor(qt.qeye(nmax), HB) + (g / np.sqrt(N)) * (qt.tensor(a, Sp) + qt.tensor(a.dag(), Sm)))
+    Sx, Sy, Sz, Sp, Sm = collective_ops(N)
+    HB = Sz + (N/2)*qt.qeye(N+1)
+    
+    H = (ω * qt.tensor(a.dag()*a, qt.qeye(N+1)) + qt.tensor(qt.qeye(nmax), HB) + (g / np.sqrt(N)) * (qt.tensor(a, Sp) + qt.tensor(a.dag(), Sm)))
     return H, HB, Sx, Sy, Sz
 
 ################
